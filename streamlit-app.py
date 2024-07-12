3ffcd789e12eeb963af7f28ebdf4012b0835aa58
Shaheer04 <sheeryjamal05@gmail.com>
2024-07-12 22:29:08 +0500

    Minor UI button element changes

--- streamlit-app.py
+++ streamlit-app.py
@@ -17,9 +17,8 @@ def response_generator(text: str):
             yield "\n\n"  # Add paragraph breaks
 
 st.title("BudgetBuddy")
-st.info("""
-        I am your Budget Buddy, Ask me anything about the latest budget and finance of Pakistan.
-        """)
+st.info("I am your Budget Buddy, Ask me anything about the latest budget and finance of Pakistan.")
+st.caption("Use buttons to ask question or type in the chat box.")
 
 def conversation(query:str):
     # Add user message to chat history
@@ -35,26 +34,38 @@ def conversation(query:str):
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+with st.container(border=True):
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if query := st.chat_input("Enter a message..."):
-    conversation(query=query)
+    if query := st.chat_input("Enter a message..."):
+        conversation(query=query)
 
 if st.button("What is the property transfer tax in this budget?", use_container_width=True, type="primary"):
-    conversation(query="What is the property transfer tax in this budget?")
+    question = "What is the property transfer tax in this budget?"
 
 if st.button(" What changes have been made to the income tax slabs and rates for individuals?",  use_container_width=True, type="primary"):
-    conversation(query="What changes have been made to the income tax slabs and rates for individuals?")
+    question = "What changes have been made to the income tax slabs and rates for individuals?"
 
-if st.button("What is the rate of withholding tax on cash withdrawal from banks for non-filers?",  use_container_width=True, type="primary"):
-    conversation(query="What is the rate of withholding tax on cash withdrawal from banks for non-filers?")
+if st.button("what is electronic invoicing system according to this budget?",  use_container_width=True, type="primary"):
+    question = "what is electronic invoicing system according to this budget?"
+
+if 'question' in locals():
+    match question:
+        case "What is the property transfer tax in this budget?":
+            conversation(query=question)
+        case "What changes have been made to the income tax slabs and rates for individuals?":
+            conversation(query=question)
+        case "what is electronic invoicing system according to this budget?":
+            conversation(query=question)
+        case _:
+            pass
 
 st.caption("Made with ❤️ by Shaheer Jamal")
 
